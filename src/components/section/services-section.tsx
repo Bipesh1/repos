bb2533eb7ebaed5ac6debbc5a7916cf31f454572
Bipesh <adbipesh001@gmail.com>
2025-04-05 11:22:22 +0545

    changes

--- src/components/section/services-section.tsx
+++ src/components/section/services-section.tsx
@@ -1,35 +1,38 @@
 import React from 'react'
-import { MdDashboard } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
-import { IoIosChatboxes } from "react-icons/io";
+import { FaUniversity } from "react-icons/fa";
+import { RiMoneyDollarBoxLine } from "react-icons/ri";
+import { FaWpforms } from "react-icons/fa6";
+
+
 
 export default function ServiceSection() {
   return (
     <section className='container mx-auto md:px-12 px-4 space-y-20 mt-20'>
         <div className='container mx-auto text-center space-y-5'>
-            <h2 className='text-3xl font-semibold'>What We<span className='text-primary'>{" "}Provide More</span></h2>
-            <p className='text-center'>Its easier than you imagine.Steps away from <span className='text-secondary'>streamlining your flow</span>.</p>
+            <h2 className='text-3xl font-semibold'>How It<span className='text-primary'>{" "}Works?</span></h2>
+            <p className='text-center'>Simple steps to apply,track and<span className='text-secondary'> secure your admissions.</span>.</p>
         </div>
         <div className='container mx-auto border-red-500 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-10 md:gap-y-5  place-items-center'>
             <div className='flex flex-col justify-center items-center space-y-4' >
                 <FaUser className='text-7xl text-primary'/>
-                <p className='text-xl font-semibold'>User <span className='text-primary'>Experience</span></p>
-                <p>Enjoy the seamless user <span className='text-secondary'>experience</span>.</p>
+                <p className='text-xl font-semibold'>Register</p>
+                <p>Create account, <span className='text-secondary'>upload documents.</span>.</p>
             </div>
             <div className='flex flex-col justify-center items-center space-y-4' >
-                <MdDashboard  className='text-7xl text-primary'/>
-                <p className='text-xl font-semibold'>Dashboard</p>
-                <p>Get Your Own <span className='text-secondary'>Dashboard</span>.</p>
+                <FaUniversity className='text-7xl text-primary'/>
+                <p className='text-xl font-semibold'>Search University</p>
+                <p>Search your universities <span className='text-secondary'>and courses.</span>.</p>
             </div>
             <div className='flex flex-col justify-center items-center space-y-4'  >
-                <IoIosChatboxes className='text-7xl text-primary'/>
-                <p className='text-xl font-semibold'>Communication <span className='text-primary'>Platform</span></p>
-                <p>Communicate with your <span className='text-secondary'>counselor</span>.</p>
+                <RiMoneyDollarBoxLine className='text-7xl text-primary'/>
+                <p className='text-xl font-semibold'>Assured <span className='text-primary'>Scholarship</span></p>
+                <p>Find Scholarships <span className='text-secondary'>Accordingly.</span>.</p>
             </div>
             <div className='flex flex-col justify-center items-center space-y-4'  >
-                <MdDashboard className='text-7xl text-primary'/>
-                <p className='text-xl font-semibold'>User <span className='text-primary'>Experience</span></p>
-                <p>Enjoy the seamless user <span className='text-secondary'>experience</span>.</p>
+                <FaWpforms className='text-7xl text-primary'/>
+                <p className='text-xl font-semibold'>Self <span className='text-primary'>Apply</span></p>
+                <p>Submit application & get <span className='text-secondary'>admission</span>.</p>
             </div>
         </div>
     </section>
